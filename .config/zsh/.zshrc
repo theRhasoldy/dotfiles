fbef06c0d4d0dde77f65099f17a656ad66680f36
NEDTawfik <rhasoldy@gmail.com>
2022-06-03 18:27:13 +0200

    Added zsh config and fixed weird prompt glitch

---
+++ .config/zsh/.zshrc
@@ -0,0 +1,5 @@
+#Load seperated config files
+for conf in "$HOME/.config/zsh/config.d/"*.zsh; do
+  source "${conf}"
+done
+unset conf
---
+++ .config/zsh/config.d/plugins.zsh
@@ -0,0 +1,12 @@
+source ~/.config/zsh/plugins/alias-tips/alias-tips.plugin.zsh
+
+source ~/.config/zsh/plugins/zsh-autopair/autopair.zsh
+
+source ~/.config/zsh/plugins/zsh-lazyload/zsh-lazyload.zsh
+
+source ~/.config/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+ZSH_AUTOSUGGEST_STRATEGY=(history completion)
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#595959,underline"
+
+source ~/.config/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
