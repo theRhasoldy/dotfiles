9a18171d991097206125d8e2ac12924015d357cd
NEDTawfik <rhasoldy@gmail.com>
2022-06-03 03:41:29 +0200

    Added zsh config

---
+++ .config/zsh/config.d/aliases.zsh
@@ -0,0 +1,12 @@
+#exa --> ls
+alias ls="exa --icons -x"
+alias lsa="ls -a"
+alias ll="exa -l --icons"
+alias lla="ll -a"
+alias llt="exa -l -T --icons"
+
+alias %=""
+
+alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
+
+alias pacman="sudo pacman"
