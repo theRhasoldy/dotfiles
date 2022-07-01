8ce0b069f2ad06469fc93d03b45d7db0bfcacedb
NEDTawfik <rhasoldy@gmail.com>
2022-07-01 23:10:31 +0200

    Quick fix

--- .config/zsh/config.d/plugins.zsh
+++ .config/zsh/config.d/plugins.zsh
@@ -14,6 +14,6 @@ ZSH_HIGHLIGHT_STYLES[command]="fg=10"
 ZSH_HIGHLIGHT_STYLES[reserved-word]="fg=12"
 ZSH_HIGHLIGHT_STYLES[alias]="fg=12"
 ZSH_HIGHLIGHT_STYLES[builtin]="fg=12"
-ZSH_HIGHLIGHT_STYLES[path]="fg=11"
-ZSH_HIGHLIGHT_STYLES[autodirectory]="fg=11"
+ZSH_HIGHLIGHT_STYLES[path]="fg=13"
+ZSH_HIGHLIGHT_STYLES[autodirectory]="fg=13"
 
