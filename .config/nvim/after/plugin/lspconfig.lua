e415814458bb71c70e27fbf5447e594a63953cb0
theRhasoldy <rhasoldy@gmail.com>
2022-10-02 23:06:43 +0200

    Gitsigns now change the numberline color instead of having its own sign column

--- .config/nvim/after/plugin/lspconfig.lua
+++ .config/nvim/after/plugin/lspconfig.lua
@@ -4,6 +4,7 @@ if not status then
 end
 
 local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local cmd = vim.api.nvim_command
 
 local lsp_flags = {
 	debounce_text_changes = 150,
@@ -17,11 +18,11 @@ end
 
 local on_attach = function(client, bufnr)
 	if client.server_capabilities.documentFormattingProvider then
-		vim.api.nvim_command([[augroup Format]])
-		vim.api.nvim_command([[autocmd! * <buffer>]])
-		vim.api.nvim_command([[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]])
-		vim.api.nvim_command([[augroup END]])
-		vim.api.nvim_command([[autocmd FileType dart setlocal shiftwidth=2 tabstop=2]])
+		cmd([[augroup Format]])
+		cmd([[autocmd! * <buffer>]])
+		cmd([[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]])
+		cmd([[augroup END]])
+		cmd([[autocmd FileType dart setlocal shiftwidth=2 tabstop=2]])
 	end
 end
 
