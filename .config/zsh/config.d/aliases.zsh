1baa24cbb489be5e578df25fb62bc8b5225294dd
theRhasoldy <rhasoldy@gmail.com>
2022-11-02 00:43:40 +0200

    Added dots thing, updated git token location and removed unused wiki alias

--- .config/zsh/config.d/aliases.zsh
+++ .config/zsh/config.d/aliases.zsh
@@ -1,9 +1,15 @@
-#exa --> ls
+#ls --> exa
 alias ls="exa --icons -x"
 alias lsa="ls -a"
 alias ll="exa -l --icons"
 alias lla="ll -a"
 alias llt="exa -l -T --icons"
+
+#../.. --> ...
+alias ...="../.."
+alias ....="../../.."
+alias .....="../../../.."
+alias ......="../../../../.."
 
 alias rm="rm -v"
 
@@ -21,6 +27,6 @@ alias wiki="cd ~/Universe && nvim -c VimwikiIndex"
 
 alias update-android="paru -Syu android-emulator android-google-apis android-ndk android-platform android-sdk android-sdk-build-tools android-google-apis-x86-64-system-image"
 
-alias git-token='echo "$(cat ~/Mind/tokens.md)" | xclip -selection clipboard && echo "Git token copied to clipboard :)"'
+alias git-token='echo "$(cat $HOME/Documents/protected/tokens.md)" | xclip -selection clipboard && echo "Git token copied to clipboard :)"'
 
 alias wget=wget --hsts-file="$XDG_DATA_HOME/wget-hsts"
