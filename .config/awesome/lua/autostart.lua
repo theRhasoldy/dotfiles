5d92a198690fc3a496500f44b19214139e77a61b
theRhasoldy <rhasoldy@gmail.com>
2022-11-19 16:09:12 +0200

    Added xbanish to autostart

--- .config/awesome/lua/autostart.lua
+++ .config/awesome/lua/autostart.lua
@@ -1,6 +1,7 @@
 local awful = require("awful")
 
 awful.spawn.with_shell("picom --experimental-backend")
+awful.spawn.with_shell("xbanish -t 5")
 awful.spawn.with_shell("lxsession")
 awful.spawn.with_shell("greenclip daemon")
 awful.spawn.with_shell("feh --bg-fill --no-fehbg /usr/share/backgrounds/background.jpg")
