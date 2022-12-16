c78cae1b51b77c4e9ea0e7ee8821dbcb1d8a5989
theRhasoldy <rhasoldy@gmail.com>
2022-12-16 04:47:47 +0200

    Todo: figure out how to autostart apps only once even after restart

--- .config/awesome/lua/autostart.lua
+++ .config/awesome/lua/autostart.lua
@@ -1,7 +1,8 @@
 local awful = require("awful")
 
 awful.spawn.with_shell("picom --experimental-backend")
-awful.spawn.with_shell("xbanish -t 5")
-awful.spawn.with_shell("lxsession")
+awful.spawn.with_shell("xset r rate 200 40")
+awful.spawn.with_shell("xbanish -t 10")
+awful.spawn.once("lxsession")
 awful.spawn.with_shell("greenclip daemon")
 awful.spawn.with_shell("feh --bg-fill --no-fehbg /usr/share/backgrounds/background.jpg")
