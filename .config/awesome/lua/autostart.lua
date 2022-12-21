f8cf27a38430e1763feb0a757d1391c5bd9333fb
theRhasoldy <rhasoldy@gmail.com>
2022-12-22 01:45:36 +0200

    Added xremap and removed xset from autostart

--- .config/awesome/lua/autostart.lua
+++ .config/awesome/lua/autostart.lua
@@ -1,7 +1,8 @@
 local awful = require("awful")
 
 awful.spawn.with_shell("picom --experimental-backend")
-awful.spawn.with_shell("xset r rate 200 40")
+-- awful.spawn.with_shell("xset r rate 200 40")
+awful.spawn.with_shell("xremap $HOME/.config/xremap.yml")
 awful.spawn.with_shell("xbanish -t 10")
 awful.spawn.once("lxsession")
 awful.spawn.with_shell("greenclip daemon")
