50e9257acf7a17112c79170c0e01f11456cc8eee
theRhasoldy <rhasoldy@gmail.com>
2023-01-03 23:25:18 +0200

    Autostart is handeled by bash script to avoid multiple instances (Can't do it in lua 🤓)

--- .config/awesome/lua/autostart.lua
+++ .config/awesome/lua/autostart.lua
@@ -1,9 +1,4 @@
 local awful = require("awful")
 
-awful.spawn.with_shell("picom --experimental-backend")
--- awful.spawn.with_shell("xset r rate 200 40")
-awful.spawn.with_shell("xremap $HOME/.config/xremap.yml")
-awful.spawn.with_shell("xbanish -t 10")
-awful.spawn.once("lxsession")
-awful.spawn.with_shell("greenclip daemon")
-awful.spawn.with_shell("feh --bg-fill --no-fehbg /usr/share/backgrounds/background.jpg")
+-- Spawn Autorun Script
+awful.spawn.with_shell(scripts_path .. "autostart.sh")
