3703b8df6e45dd48bb2489975e6408287f8fd89a
theRhasoldy <rhasoldy@gmail.com>
2023-01-04 04:07:28 +0200

    Simpler rc file

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -1,27 +1,25 @@
 local gears = require("gears")
 local awful = require("awful")
-local naughty = require("naughty")
 local beautiful = require("beautiful")
+local theme = require("theme")
 
 pcall(require, "luarocks.loader")
-
-local theme = require("theme")
 
 home_path = os.getenv("HOME") .. "/"
 scripts_path = home_path .. ".config/scripts/"
 
+-- Load Modules {{{
 require("lua.autostart")
 require("lua.wibar")
 require("lua.keybinds")
 require("lua.clients")
 require("lua.notifications")
 require("awful.hotkeys_popup.keys")
+-- }}}
 
 -- Theme {{{
 beautiful.init(gears.filesystem.get_configuration_dir() .. "theme.lua")
 -- }}}
-
--- awful.layout.suit.tile.mirror = true
 
 -- Layouts
 awful.layout.layouts = {
