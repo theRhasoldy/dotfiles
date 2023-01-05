90fbe8cff866e547fe09e7a2e663e358b4ac53cc
theRhasoldy <rhasoldy@gmail.com>
2023-01-05 03:02:17 +0200

    Dump now goes to .cache

--- .config/zsh/config.d/history.zsh
+++ .config/zsh/config.d/history.zsh
@@ -3,12 +3,22 @@ HISTFILE=~/.cache/zsh/hist
 HISTSIZE=10000
 SAVEHIST=10000
 
-setopt SHARE_HISTORY
-setopt EXTENDED_HISTORY
+setopt EXTENDED_HISTORY          
+setopt SHARE_HISTORY             
+setopt HIST_EXPIRE_DUPS_FIRST    
+setopt HIST_IGNORE_DUPS          
+setopt HIST_IGNORE_ALL_DUPS      
+setopt HIST_FIND_NO_DUPS         
+setopt HIST_IGNORE_SPACE         
+setopt HIST_SAVE_NO_DUPS         
+setopt HIST_VERIFY               
 
-#No duplicates
-setopt HIST_EXPIRE_DUPS_FIRST
-setopt HIST_IGNORE_ALL_DUPS
-setopt HIST_IGNORE_SPACE
-setopt HIST_FIND_NO_DUPS
-setopt HIST_SAVE_NO_DUPS
+# Execute code that does not affect the current session in the background.
+{
+  # Compile the completion dump to increase startup speed.
+  zcompdump="${XDG_CACHE_HOME:-$HOME/.cache}/zsh/zcompdump"
+  if [[ -s "$zcompdump" && (! -s "${zcompdump}.zwc" || "$zcompdump" -nt "${zcompdump}.zwc") ]]; then
+    zcompile "$zcompdump"
+  fi
+} &!
+
