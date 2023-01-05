7d9e6a196ae7b54f2b554b8b1fefb65b4e8f3a8a
theRhasoldy <rhasoldy@gmail.com>
2023-01-05 03:03:02 +0200

    New aliases taken from fish config

--- .config/zsh/config.d/aliases.zsh
+++ .config/zsh/config.d/aliases.zsh
@@ -11,16 +11,28 @@ alias ....="../../.."
 alias .....="../../../.."
 alias ......="../../../../.."
 
-alias rm="rm -v"
-
-alias dotfiles="/usr/bin/git --git-dir=$HOME/.config/dotfiles/ --work-tree=$HOME"
-
 alias pacman="sudo pacman"
-
-alias suspend="systemctl suspend"
 
 alias matrix="unimatrix -c magenta -s 96 -a -l s"
 
 alias update-android="paru -Syu android-emulator android-google-apis android-ndk android-platform android-sdk android-sdk-build-tools android-google-apis-x86-64-system-image"
 
 alias git-token='echo "$(cat $HOME/Documents/protected/tokens.md)" | xclip -selection clipboard && echo "Git token copied to clipboard :)"'
+
+alias rm="rm -vI"
+alias cp="rsync -ahv --info=progress2"
+alias mv="mv -vi"
+
+alias grep="grep --color=auto"
+
+# Nvim 
+alias vim="nvim"
+alias vi="nvim"
+alias nano="nvim"
+
+# Dotfiles
+alias dot="/usr/bin/git --git-dir=$HOME/.config/dotfiles/ --work-tree=$HOME"
+alias dots="dot status"
+alias dota="dot add"
+alias dotc="dot commit -m"
+
