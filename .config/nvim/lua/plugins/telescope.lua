a0c81359266ffcaf7eafe314a0b6e4ba88ffb957
theRhasoldy <rhasoldy@gmail.com>
2023-01-22 02:17:35 +0200

    Added telescope for fuzzy search

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,7 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
+    name = "telescope",
     dependencies = { 
       "nvim-lua/plenary.nvim",
       "nvim-telescope/telescope-live-grep-args.nvim",
