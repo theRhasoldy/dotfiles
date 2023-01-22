4f0572a06bc5f826a8aad9e22ed2b6e89af49cd2
theRhasoldy <rhasoldy@gmail.com>
2023-01-22 02:18:28 +0200

    Added neo-tree

--- .config/nvim/lua/plugins/tree.lua
+++ .config/nvim/lua/plugins/tree.lua
@@ -1,14 +1,63 @@
+local cmd = vim.cmd
+cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+
 return {
   {
     "nvim-neo-tree/neo-tree.nvim",
+    name = "neo-tree",
     dependencies = {
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "nvim-tree/nvim-web-devicons",
       "MunifTanjim/nui.nvim",
     },
+    opts = {
+      close_if_last_window = true,
+      popup_border_style = "rounded",
+      enable_git_status = true,
+      enable_diagnostics = true,
+      filesystem = {
+        filtered_items = {
+          visible = true, -- when true, they will just be displayed differently than normal items
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+        follow_current_file = true,
+        hijack_netrw_behavior = "open_current",
+        follow_current_file = true,
+        use_libuv_file_watcher = true, -- This will use the OS level file watchers to detect changes
+      },
+      buffers = {
+        follow_current_file = true,
+        show_unloaded = true,
+      },
+      window = {
+        mappings = {
+          ["q"] = "revert_preview",
+          ["<esc>"] = "close_window",
+        },
+      },
+      default_component_configs = {
+        icon = {
+          folder_closed = "",
+          folder_open = "",
+          folder_empty = "",
+          default = "󰧮",
+          highlight = "NeoTreeFileIcon"
+        },
+        modified = {
+          symbol = " ",
+          highlight = "NeoTreeModified",
+        },
+        indent = {
+          with_markers = false,
+          with_expanders = true,
+          expander_highlight = "NeoTreeExpander",
+        },
+      },
+    },
     config = function(_, opts)
-      require("neo-tree").setup({
-        hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
-      })
+      require("neo-tree").setup(opts)
+      vim.keymap.set("n", "<Leader>n", "<cmd>:Neotree float<CR>")
     end,
   }
 }
+
