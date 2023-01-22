1c7766b043dc7f8d5e55915c095f6404d239dcc3
theRhasoldy <rhasoldy@gmail.com>
2023-01-22 16:46:06 +0200

    Switched tag 5 with 3

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -30,5 +30,5 @@ awful.layout.layouts = {
 -- }}}
 
 -- Tags {{{
-awful.tag({ "", "", "", "", "", "", "" }, s, awful.layout.layouts[1])
+awful.tag({ "", "", "", "", "", "", "" }, s, awful.layout.layouts[1])
 -- }}}
