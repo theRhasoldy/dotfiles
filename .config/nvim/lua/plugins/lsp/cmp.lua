fc856b60706431bfc66b772640bf57373708f956
theRhasoldy <rhasoldy@gmail.com>
2023-01-27 21:17:00 +0200

    Added friendly snippets

--- .config/nvim/lua/plugins/lsp/cmp.lua
+++ .config/nvim/lua/plugins/lsp/cmp.lua
@@ -1,74 +1,78 @@
 return {
-	"hrsh7th/nvim-cmp",
-	name = "cmp",
-	lazy = true,
-	event = "BufReadPre",
-	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-path",
-		"L3MON4D3/LuaSnip",
-		"saadparwaiz1/cmp_luasnip",
-	},
-	opts = function()
-		local cmp = require("cmp")
-		return {
-			-- Disable cmp in comments
-			enabled = function()
-				local in_prompt = vim.api.nvim_buf_get_option(0, "buftype") == "prompt"
-				if in_prompt then -- this will disable cmp in the Telescope window (taken from the default config)
-					return false
-				end
-				local context = require("cmp.config.context")
-				return not (context.in_treesitter_capture("comment") == true or context.in_syntax_group("Comment"))
-			end,
+  "hrsh7th/nvim-cmp",
+  name = "cmp",
+  lazy = true,
+  event = "BufReadPre",
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip",
+    "rafamadriz/friendly-snippets",
+  },
+  opts = function()
+    require("luasnip/loaders/from_vscode").lazy_load()
+    require("luasnip").filetype_extend("all", { "_)" })
 
-			completion = {
-				completeopt = "menu,menuone,noinsert",
-			},
+    local cmp = require("cmp")
+    return {
+      -- Disable cmp in comments
+      enabled = function()
+        local in_prompt = vim.api.nvim_buf_get_option(0, "buftype") == "prompt"
+        if in_prompt then -- this will disable cmp in the Telescope window (taken from the default config)
+          return false
+        end
+        local context = require("cmp.config.context")
+        return not (context.in_treesitter_capture("comment") == true or context.in_syntax_group("Comment"))
+      end,
 
-			window = {
-				documentation = cmp.config.window.bordered({
-					winhighlight = "Normal:Normal,FloatBorder:BorderBG,CursorLine:PmenuSel,Search:None,PmenuThumb:BorderBG",
-				}),
-				completion = cmp.config.window.bordered({
-					winhighlight = "Normal:Normal,FloatBorder:BorderBG,CursorLine:PmenuSel,Search:None",
-				}),
-			},
+      completion = {
+        completeopt = "menu,menuone,noinsert",
+      },
 
-			snippet = {
-				expand = function(args)
-					require("luasnip").lsp_expand(args.body)
-				end,
-			},
+      window = {
+        documentation = cmp.config.window.bordered({
+          winhighlight = "Normal:Normal,FloatBorder:BorderBG,CursorLine:PmenuSel,Search:None,PmenuThumb:BorderBG",
+        }),
+        completion = cmp.config.window.bordered({
+          winhighlight = "Normal:Normal,FloatBorder:BorderBG,CursorLine:PmenuSel,Search:None",
+        }),
+      },
 
-			mapping = cmp.mapping.preset.insert({
-				["<C-c>"] = cmp.mapping.complete(),
-				["<C-e>"] = cmp.mapping.abort(),
-				["<CR>"] = cmp.mapping.confirm({ select = true }),
-			}),
+      snippet = {
+        expand = function(args)
+          require("luasnip").lsp_expand(args.body)
+        end,
+      },
 
-			sources = cmp.config.sources({
-				{ name = "nvim_lsp" },
-				{ name = "nvim_lsp_signature_help" },
-				{ name = "luasnip" },
-				{ name = "buffer", keyword_length = 5, max_item_count = 10 },
-				{ name = "path" },
-			}),
+      mapping = cmp.mapping.preset.insert({
+        ["<C-c>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+      }),
 
-			formatting = {
-				format = require("lspkind").cmp_format({
-					mode = "symbol",
-					maxwidth = 100,
-					ellipsis_char = "...",
-				}),
-			},
+      sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "nvim_lsp_signature_help" },
+        { name = "luasnip" },
+        { name = "buffer", keyword_length = 5, max_item_count = 10 },
+        { name = "path" },
+      }),
 
-			experimental = {
-				ghost_text = {
-					hl_group = "Title",
-				},
-			},
-		}
-	end,
+      formatting = {
+        format = require("lspkind").cmp_format({
+          mode = "symbol",
+          maxwidth = 100,
+          ellipsis_char = "...",
+        }),
+      },
+
+      experimental = {
+        ghost_text = {
+          hl_group = "Title",
+        },
+      },
+    }
+  end,
 }
