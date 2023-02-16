d9a71b3d047ef732001b0100fc7bb90e7541b353
theRhasoldy <rhasoldy@gmail.com>
2023-02-16 20:27:18 +0200

    Added autocmd to change Format Options

--- .config/nvim/lua/therhasoldy/autocmds.lua
+++ .config/nvim/lua/therhasoldy/autocmds.lua
@@ -1,8 +1,18 @@
--- local api = vim.api
+local api = vim.api
 
--- local Finder = api.nvim_create_augroup("Finder", { clear = true })
--- api.nvim_clear_autocmds({ group = Finder })
--- api.nvim_create_autocmd("VimEnter", {
---   command = "if isdirectory(expand('%')) | exec 'Telescope file_browser cwd=' . expand('%') | endif",
---   group = Finder,
--- })
+-- Auto reload files to detect changes
+local AutoReload = api.nvim_create_augroup("AutoReload", { clear = true })
+api.nvim_clear_autocmds({ group = AutoReload })
+api.nvim_create_autocmd({ "FocusGained", "BufEnter" }, {
+	command = ":checktime",
+	group = AutoReload,
+})
+
+-- Format Options
+local FormatOptions = api.nvim_create_augroup("FormatOptions", { clear = true })
+api.nvim_clear_autocmds({ group = FormatOptions })
+api.nvim_create_autocmd("BufEnter", {
+	callback = function()
+		vim.opt.formatoptions = vim.opt.formatoptions - { "c", "r", "o" }
+	end,
+})
