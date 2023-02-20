919eaf2a39f300ca56628c0fbb99c168c1b58036
theRhasoldy <rhasoldy@gmail.com>
2023-02-20 11:22:41 +0200

    Added which-key

--- .config/nvim/lua/plugins/utils/navigate.lua
+++ .config/nvim/lua/plugins/utils/navigate.lua
@@ -1,39 +1,54 @@
 return {
-	"knubie/vim-kitty-navigator",
-	event = "UIEnter",
-	keys = {
-		{
-			"<C-h>",
-			"<cmd>KittyNavigateLeft<CR>",
+	{
+		"knubie/vim-kitty-navigator",
+		event = "UIEnter",
+		keys = {
 			{
-				silent = true,
-				noremap = true,
+				"<C-h>",
+				"<cmd>KittyNavigateLeft<CR>",
+				{
+					silent = true,
+					noremap = true,
+				},
 			},
-		},
-		{
-			"<C-l>",
-			"<cmd>KittyNavigateRight<CR>",
 			{
-				silent = true,
-				noremap = true,
+				"<C-l>",
+				"<cmd>KittyNavigateRight<CR>",
+				{
+					silent = true,
+					noremap = true,
+				},
 			},
-		},
 
-		{
-			"<C-k>",
-			"<cmd>KittyNavigateUp<CR>",
 			{
-				silent = true,
-				noremap = true,
+				"<C-k>",
+				"<cmd>KittyNavigateUp<CR>",
+				{
+					silent = true,
+					noremap = true,
+				},
+			},
+			{
+				"<C-j>",
+				"<cmd>KittyNavigateDown<CR>",
+				{
+					silent = true,
+					noremap = true,
+				},
 			},
 		},
-		{
-			"<C-j>",
-			"<cmd>KittyNavigateDown<CR>",
-			{
-				silent = true,
-				noremap = true,
-			},
-		},
+	},
+	{
+		"folke/which-key.nvim",
+		event = "VimEnter",
+		config = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+			require("which-key").setup({
+				window = {
+					border = "single",
+				},
+			})
+		end,
 	},
 }
