dcb95d15f8576dce5b06bd981f95ee1aaeecde2a
theRhasoldy <rhasoldy@gmail.com>
2023-02-20 11:26:17 +0200

    Added colorpicker

--- .config/nvim/lua/plugins/utils/colorizer.lua
+++ .config/nvim/lua/plugins/utils/colorizer.lua
@@ -1,17 +1,29 @@
 return {
-	"NvChad/nvim-colorizer.lua",
-	name = "colorizer",
-	event = "BufReadPre",
-	opts = {
-		filetypes = { "*", "!TelescopePrompt", "!cmp_docs", "!cmp_menu", "!noice", "!notify" },
-		user_default_options = {
-			names = false, -- "Name" codes like Blue or blue
-			RRGGBBAA = true,
-			AARRGGBB = true,
-			css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-			mode = "virtualtext",
-			tailwind = "lsp",
-			sass = { enable = true, parsers = { css } }, -- Enable sass colors
+
+	{
+		"NvChad/nvim-colorizer.lua",
+		name = "colorizer",
+		event = "BufReadPre",
+		opts = {
+			filetypes = { "*", "!TelescopePrompt", "!cmp_docs", "!cmp_menu", "!noice", "!notify" },
+			user_default_options = {
+				names = false, -- "Name" codes like Blue or blue
+				RRGGBBAA = true,
+				AARRGGBB = true,
+				css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+				mode = "virtualtext",
+				tailwind = "lsp",
+				sass = { enable = true, parsers = { css } }, -- Enable sass colors
+			},
+		},
+	},
+	{
+		"ziontee113/color-picker.nvim",
+		event = "BufReadPre",
+		-- name = "color-picker",
+		opts = {},
+		keys = {
+			{ "gp", "<cmd>PickColor<cr>", noremap = true },
 		},
 	},
 }
--- .config/nvim/lua/plugins/utils/colorscheme.lua
+++ .config/nvim/lua/plugins/utils/colorscheme.lua
@@ -1,45 +1,39 @@
 return {
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "dahlia",
-		},
-	},
-	{
-		name = "dahlia",
-		lazy = false,
-		priority = 1000,
-		dev = true,
-		dir = "~/Projects/Lua/nvim-dahlia/",
-		config = function()
-			vim.cmd.colorscheme("dahlia")
-		end,
-	},
-	--	{
-	--		"unarvim/horizon.nvim",
-	--  },
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		opts = {
-			transparent_background = true,
-			color_overrides = {
-				mocha = {
-					text = "#F4CDE9",
-					subtext1 = "#DEBAD4",
-					subtext0 = "#C8A6BE",
-					overlay2 = "#B293A8",
-					overlay1 = "#9C7F92",
-					overlay0 = "#866C7D",
-					surface2 = "#705867",
-					surface1 = "#5A4551",
-					surface0 = "#44313B",
+  {
+    name = "dahlia",
+    lazy = false,
+    priority = 1000,
+    dev = true,
+    dir = "~/Projects/Lua/nvim-dahlia/",
+    config = function()
+      vim.cmd.colorscheme("dahlia")
+    end,
+  },
+  --	{
+  --		"unarvim/horizon.nvim",
+  --  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      transparent_background = true,
+      color_overrides = {
+        mocha = {
+          text = "#F4CDE9",
+          subtext1 = "#DEBAD4",
+          subtext0 = "#C8A6BE",
+          overlay2 = "#B293A8",
+          overlay1 = "#9C7F92",
+          overlay0 = "#866C7D",
+          surface2 = "#705867",
+          surface1 = "#5A4551",
+          surface0 = "#44313B",
 
-					base = "#352939",
-					mantle = "#211924",
-					crust = "#1a1016",
-				},
-			},
-		},
-	},
+          base = "#352939",
+          mantle = "#211924",
+          crust = "#1a1016",
+        },
+      },
+    },
+  },
 }
