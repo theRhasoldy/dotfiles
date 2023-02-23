eb17d1359695d84cf8d3f7c261e0aec470f229b0
theRhasoldy <rhasoldy@gmail.com>
2023-02-23 04:31:00 +0200

    Made which-key lazy

--- .config/nvim/lua/plugins/utils/init.lua
+++ .config/nvim/lua/plugins/utils/init.lua
@@ -1,6 +1,5 @@
 return {
 	require("plugins.utils.colorscheme"),
-	require("plugins.utils.quick"),
 	require("plugins.utils.colorizer"),
 	require("plugins.utils.navigate"),
 	require("plugins.utils.filetype"),
