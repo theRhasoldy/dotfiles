be80673b1ad4049fcd54ed0a000de4f85de6c4df
theRhasoldy <rhasoldy@gmail.com>
2023-02-28 16:51:15 +0200

    Added harpoon

---
+++ .config/nvim/lua/plugins/ui/harpoon.lua
@@ -0,0 +1,23 @@
+return {
+  "ThePrimeagen/harpoon",
+  opts = {
+    global_settings = {
+      excluded_filetypes = { "harpoon", "TelescopePrompt", "cmp_docs", "cmp_menu", "noice", "notify" },
+    },
+  },
+  keys = {
+    {
+      "m",
+      function()
+        require("harpoon.mark").add_file()
+        print("Mark saved!")
+      end,
+    },
+    {
+      "<Leader>m",
+      function()
+        require("telescope").extensions.harpoon.marks()
+      end,
+    },
+  },
+}
