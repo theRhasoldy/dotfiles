4d5f243ed031f2c99b1d0ef856a39d5b88ddd244
Nour El Din Shady <rhasoldy@gmail.com>
2023-03-06 11:56:10 +0200

    Added indent scope

--- .config/nvim/lua/plugins/utils/init.lua
+++ .config/nvim/lua/plugins/utils/init.lua
@@ -1,6 +1,6 @@
 return {
-	require("plugins.utils.colorscheme"),
-	require("plugins.utils.colorizer"),
-	require("plugins.utils.navigate"),
-	require("plugins.utils.filetype"),
+  require("plugins.utils.colorscheme"),
+  require("plugins.utils.colorizer"),
+  require("plugins.utils.navigate"),
+  require("plugins.utils.filetype"),
 }
