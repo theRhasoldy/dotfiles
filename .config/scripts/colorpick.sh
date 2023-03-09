916868ef143c101335d6616b378ea5385330e2b6
Nour El Din Shady <rhasoldy@gmail.com>
2023-03-09 18:54:06 +0200

    Finally fixed newline when pasting color

--- .config/scripts/colorpick.sh
+++ .config/scripts/colorpick.sh
@@ -1,4 +1,5 @@
+#!/usr/bin/env bash
 cmd=$(colorpicker --one-shot --short --preview)
-echo $cmd | xclip -selection clipboard
+echo -n $cmd | xclip -selection clipboard
 notify-send --icon /usr/share/icons/Fluent-grey-dark/scalable/apps/color.svg -a "Colorpicker" "Hex-code" $cmd
 exit 0 
