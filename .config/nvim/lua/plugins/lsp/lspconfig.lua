bf42419771c8d8a8c6312d0ad61f466f8809e31f
Nour El Din Shady <rhasoldy@gmail.com>
2023-04-02 13:47:05 +0200

    Fixed lua lsp

--- .config/nvim/lua/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/plugins/lsp/lspconfig.lua
@@ -1,224 +1,224 @@
 local symbols = {
-  Text = "󰊄",
-  Method = "",
-  Function = "",
-  Constructor = "󱊈",
-  Field = "ﰠ",
-  Variable = "",
-  Class = "ﴯ",
-  Interface = "",
-  Module = "",
-  Property = "ﰠ",
-  Unit = "塞",
-  Value = "",
-  Enum = "",
-  Keyword = "",
-  Snippet = "󰆐",
-  Color = "󰌁",
-  File = "󰈔",
-  Reference = "",
-  Folder = "",
-  EnumMember = "",
-  Constant = "",
-  Struct = "פּ",
-  Event = "",
-  Operator = "󱌣",
-  TypeParameter = "",
+	Text = "󰊄",
+	Method = "",
+	Function = "",
+	Constructor = "󱊈",
+	Field = "ﰠ",
+	Variable = "",
+	Class = "ﴯ",
+	Interface = "",
+	Module = "",
+	Property = "ﰠ",
+	Unit = "塞",
+	Value = "",
+	Enum = "",
+	Keyword = "",
+	Snippet = "󰆐",
+	Color = "󰌁",
+	File = "󰈔",
+	Reference = "",
+	Folder = "",
+	EnumMember = "",
+	Constant = "",
+	Struct = "פּ",
+	Event = "",
+	Operator = "󱌣",
+	TypeParameter = "",
 }
 
 local path = vim.split(package.path, ";")
 
 return {
-  {
-    "neovim/nvim-lspconfig",
-    name = "lspconfig",
-    event = { "BufReadPost", "BufNewFile" },
-    opts = {
-      -- Global settings
-      diagnostics = {
-        underline = true,
-        update_in_insert = true,
-        severity_sort = true,
-      },
-      autoformat = false,
-    },
+	{
+		"neovim/nvim-lspconfig",
+		name = "lspconfig",
+		event = { "BufReadPost", "BufNewFile" },
+		opts = {
+			-- Global settings
+			diagnostics = {
+				underline = true,
+				update_in_insert = true,
+				severity_sort = true,
+			},
+			autoformat = false,
+		},
 
-    config = function(_, opts)
-      -- Custom sign column diagnostics icon
-      local signs = { Error = " ", Warn = " ", Hint = "", Info = " " }
-      for type, icon in pairs(signs) do
-        local hl = "DiagnosticSign" .. type
-        vim.fn.sign_define(hl, { text = icon, texthl = hl })
-      end
+		config = function(_, opts)
+			-- Custom sign column diagnostics icon
+			local signs = { Error = " ", Warn = " ", Hint = "", Info = " " }
+			for type, icon in pairs(signs) do
+				local hl = "DiagnosticSign" .. type
+				vim.fn.sign_define(hl, { text = icon, texthl = hl })
+			end
 
-      local capabilities =
-          require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
+			local capabilities =
+				require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-      -- Folding
-      capabilities.textDocument.foldingRange = {
-        dynamicRegistration = false,
-        lineFoldingOnly = true,
-      }
+			-- Folding
+			capabilities.textDocument.foldingRange = {
+				dynamicRegistration = false,
+				lineFoldingOnly = true,
+			}
 
-      local on_attach = function(client)
-        -- Disabled built-in lsp formatting, handled by null-ls
-        client.server_capabilities.documentFormattingProvider = false
-        client.server_capabilities.documentRangeFormattingProvider = false
-        -- client.server_capabilities.semanticTokensProvider = nil
-      end
+			local on_attach = function(client)
+				-- Disabled built-in lsp formatting, handled by null-ls
+				client.server_capabilities.documentFormattingProvider = false
+				client.server_capabilities.documentRangeFormattingProvider = false
+				-- client.server_capabilities.semanticTokensProvider = nil
+			end
 
-      local defaults = {
-        capabilities = capabilities,
-        on_attach = on_attach,
-      }
+			local defaults = {
+				capabilities = capabilities,
+				on_attach = on_attach,
+			}
 
-      local lsp = require("lspconfig")
+			local lsp = require("lspconfig")
 
-      lsp["lua_ls"].setup({
-        defaults,
-        single_file_support = true,
-        settings = {
-          Lua = {
-            runtime = {
-              version = "luajit",
-              -- Setup your lua path
-              path = path,
-            },
-            workspace = {
-              library = {
-                vim.api.nvim_get_runtime_file("", true),
-                [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-                [vim.fn.stdpath("config") .. "/lua"] = true,
-              },
-              -- maxPreload = 2000,
-              -- preloadFileSize = 50000,
-              checkThirdParty = true,
-            },
-            completion = {
-              workspaceWord = true,
-              callSnippet = "Both",
-            },
-            telemetry = {
-              enable = false,
-            },
-          },
-        },
-      })
+			lsp["lua_ls"].setup({
+				defaults,
+				single_file_support = true,
+				settings = {
+					Lua = {
+						runtime = {
+							version = "luajit",
+							-- Setup your lua path
+							path = path,
+						},
+						workspace = {
+							library = {
+								vim.api.nvim_get_runtime_file("", true),
+								[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+								[vim.fn.stdpath("config") .. "/lua"] = true,
+							},
+							-- maxPreload = 2000,
+							-- preloadFileSize = 50000,
+							checkThirdParty = true,
+						},
+						completion = {
+							workspaceWord = true,
+							callSnippet = "Both",
+						},
+						telemetry = {
+							enable = false,
+						},
+					},
+				},
+			})
 
-      lsp["bashls"].setup({
-        single_file_support = true,
-        cmd = { "bash-language-server", "start" },
-        filetypes = { "sh" },
-      })
+			lsp["bashls"].setup({
+				single_file_support = true,
+				cmd = { "bash-language-server", "start" },
+				filetypes = { "sh" },
+			})
 
-      lsp["tsserver"].setup(defaults)
+			lsp["tsserver"].setup(defaults)
 
-      lsp["html"].setup(defaults)
+			lsp["html"].setup(defaults)
 
-      lsp["emmet_ls"].setup(defaults)
+			lsp["emmet_ls"].setup(defaults)
 
-      lsp["yamlls"].setup(defaults)
+			lsp["yamlls"].setup(defaults)
 
-      lsp["jsonls"].setup(defaults)
+			lsp["jsonls"].setup(defaults)
 
-      lsp["cssls"].setup(defaults)
+			lsp["cssls"].setup(defaults)
 
-      lsp["dartls"].setup({
-        capabilities = capabilities,
-        on_attach = function(client)
-          -- Formatting
-          if client.server_capabilities.documentFormattingProvider then
-            vim.api.nvim_create_autocmd("BufWritePre", {
-              callback = function()
-                vim.lsp.buf.format({ async = false })
-              end,
-            })
-          end
-        end,
-      })
+			lsp["dartls"].setup({
+				capabilities = capabilities,
+				on_attach = function(client)
+					-- Formatting
+					if client.server_capabilities.documentFormattingProvider then
+						vim.api.nvim_create_autocmd("BufWritePre", {
+							callback = function()
+								vim.lsp.buf.format({ async = false })
+							end,
+						})
+					end
+				end,
+			})
 
-      vim.diagnostic.config({
-        virtual_text = false, -- Disable virtual text
-        severity_sort = true, -- sorts diagnostics by severity (errors first)
-      })
-    end,
-  },
-  {
-    "glepnir/lspsaga.nvim",
-    name = "lspsaga",
-    event = "BufRead",
-    opts = {
-      diagnostic = {
-        keys = {
-          quit = "<Esc>",
-        },
-      },
-      lightbulb = {
-        virtual_text = true,
-        sign = false,
-      },
-      symbol_in_winbar = {
-        enable = false,
-      },
-      rename = {
-        quit = "q",
-      },
-      ui = {
-        border = "single",
-        theme = "none",
-        code_action = "",
-        diagnostic = " ",
-        colors = {
-          normal_bg = "#141313",
-        },
-      },
-      kind = symbols,
-    },
-    keys = {
-      {
-        "<Leader><Leader>",
-        "<cmd>Lspsaga show_line_diagnostics<CR>",
-        { noremap = true },
-      },
-      {
-        "gr",
-        "<cmd>Lspsaga rename<CR>",
-      },
-      {
-        "ga",
-        "<cmd>Lspsaga code_action<CR>",
-      },
-      {
-        "gd",
-        "<cmd>Lspsaga hover_doc<CR>",
-      },
-      {
-        "gtd",
-        "<cmd>Lspsaga goto_definition<CR>",
-      },
-    },
-    config = function(_, opts)
-      local present, saga = pcall(require, "lspsaga")
-      if not present then
-        return
-      end
-      saga.setup(opts)
-    end,
-  },
-  {
-    "onsails/lspkind.nvim",
-    name = "lspkind",
-    event = "BufReadPre",
-    config = function()
-      local present, kind = pcall(require, "lspkind")
-      if not present then
-        return
-      end
-      kind.init({
-        mode = "symbol",
-        preset = "codicons",
-        symbol_map = symbols,
-      })
-    end,
-  },
+			vim.diagnostic.config({
+				virtual_text = false, -- Disable virtual text
+				severity_sort = true, -- sorts diagnostics by severity (errors first)
+			})
+		end,
+	},
+	{
+		"glepnir/lspsaga.nvim",
+		name = "lspsaga",
+		event = "BufRead",
+		opts = {
+			diagnostic = {
+				keys = {
+					quit = "<Esc>",
+				},
+			},
+			lightbulb = {
+				virtual_text = true,
+				sign = false,
+			},
+			symbol_in_winbar = {
+				enable = false,
+			},
+			rename = {
+				quit = "q",
+			},
+			ui = {
+				border = "single",
+				theme = "none",
+				code_action = "",
+				diagnostic = " ",
+				colors = {
+					normal_bg = "#141313",
+				},
+			},
+			kind = symbols,
+		},
+		keys = {
+			{
+				"<Leader><Leader>",
+				"<cmd>Lspsaga show_line_diagnostics<CR>",
+				{ noremap = true },
+			},
+			{
+				"gr",
+				"<cmd>Lspsaga rename<CR>",
+			},
+			{
+				"ga",
+				"<cmd>Lspsaga code_action<CR>",
+			},
+			{
+				"gd",
+				"<cmd>Lspsaga hover_doc<CR>",
+			},
+			{
+				"gtd",
+				"<cmd>Lspsaga goto_definition<CR>",
+			},
+		},
+		config = function(_, opts)
+			local present, saga = pcall(require, "lspsaga")
+			if not present then
+				return
+			end
+			saga.setup(opts)
+		end,
+	},
+	{
+		"onsails/lspkind.nvim",
+		name = "lspkind",
+		event = "BufReadPre",
+		config = function()
+			local present, kind = pcall(require, "lspkind")
+			if not present then
+				return
+			end
+			kind.init({
+				mode = "symbol",
+				preset = "codicons",
+				symbol_map = symbols,
+			})
+		end,
+	},
 }
