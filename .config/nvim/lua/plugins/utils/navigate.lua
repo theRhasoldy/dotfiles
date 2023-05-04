22963a75ba21254f82cbdb871fe6b857ebe28def
Nour El Din Shady <rhasoldy@gmail.com>
2023-05-04 15:51:02 +0300

    Removed kitty navigator entirely

--- .config/nvim/lua/plugins/utils/navigate.lua
+++ .config/nvim/lua/plugins/utils/navigate.lua
@@ -1,43 +1,4 @@
 return {
-  --[[ {
-    "knubie/vim-kitty-navigator",
-    event = "VeryLazy",
-    keys = {
-      {
-        "<C-h>",
-        "<cmd>KittyNavigateLeft<CR>",
-        {
-          silent = true,
-          noremap = true,
-        },
-      },
-      {
-        "<C-l>",
-        "<cmd>KittyNavigateRight<CR>",
-        {
-          silent = true,
-          noremap = true,
-        },
-      },
-
-      {
-        "<C-k>",
-        "<cmd>KittyNavigateUp<CR>",
-        {
-          silent = true,
-          noremap = true,
-        },
-      },
-      {
-        "<C-j>",
-        "<cmd>KittyNavigateDown<CR>",
-        {
-          silent = true,
-          noremap = true,
-        },
-      },
-    },
-  }, ]]
   {
     "christoomey/vim-tmux-navigator",
     event = "VeryLazy",
