88a4f8533a2777782106dd1ac07f55c8d3f4ff2c
Nour El Din Shady <rhasoldy@gmail.com>
2023-07-09 04:27:02 +0300

    Added early retirement plugin to manage inactive buffers

--- .config/nvim/lua/plugins/utils/init.lua
+++ .config/nvim/lua/plugins/utils/init.lua
@@ -3,4 +3,6 @@ return {
   require("plugins.utils.colorizer"),
   require("plugins.utils.navigate"),
   require("plugins.utils.filetype"),
+  require("plugins.utils.filetype"),
+  require("plugins.utils.retirement"),
 }
