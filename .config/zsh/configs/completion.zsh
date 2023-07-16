9ace1a5583ca3956ac97b02fcc2c87a25c3f403d
Nour El Din Shady <rhasoldy@gmail.com>
2023-07-16 19:59:40 +0300

    Changed location of zcompdump

--- .config/zsh/configs/completion.zsh
+++ .config/zsh/configs/completion.zsh
@@ -1,10 +1,10 @@
 #!/usr/bin/env zsh
 autoload -Uz compinit
 
-if [[ "$(date '+%j')" != "$(/usr/bin/stat -c '%Y' ${ZDOTDIR:-$HOME}/.zcompdump)" ]]; then
-  compinit
+if [[ "$(date '+%j')" != "$(/usr/bin/stat -c '%Y' ${XDG_CACHE_HOME}/zsh/.zcompdump)" ]]; then
+  compinit -d ${XDG_CACHE_HOME}/zsh/.zcompdump
 else
-  compinit -C
+  compinit -C -d ${XDG_CACHE_HOME}/zsh/.zcompdump
 fi
 
 _comp_options+=(globdots)
