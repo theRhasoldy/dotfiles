6e34378edd35290817831c8c9f386d791410dfa3
Nour El Din Shady <rhasoldy@gmail.com>
2023-07-21 14:47:47 +0300

    Added new tag and autostart

--- .config/awesome/lua/ui/layouts.lua
+++ .config/awesome/lua/ui/layouts.lua
@@ -4,16 +4,16 @@ local wibox = require("wibox")
 local bling = require("bling")
 
 tag.connect_signal("request::default_layouts", function()
-  awful.layout.append_default_layouts({
-    awful.layout.suit.tile,
-    awful.layout.suit.fair,
-    bling.layout.mstab,
-    awful.layout.suit.max,
-    bling.layout.deck,
-  })
+	awful.layout.append_default_layouts({
+		awful.layout.suit.tile,
+		awful.layout.suit.fair,
+		bling.layout.mstab,
+		awful.layout.suit.max,
+		bling.layout.deck,
+	})
 end)
 -- }}}
 
 screen.connect_signal("request::desktop_decoration", function(s)
-  awful.tag({ "", "", "", "", "", "" }, s, awful.layout.layouts[1])
+	awful.tag({ "", "", "", "󱏲", "", "", "" }, s, awful.layout.layouts[1])
 end)
--- .config/awesome/lua/utils/autostart.lua
+++ .config/awesome/lua/utils/autostart.lua
@@ -1,10 +1,10 @@
 local awful = require("awful")
 
 awful.spawn.with_shell(
-  'if (xrdb -query | grep -q "^awesome\\.started:\\s*true$"); then exit; fi;'
-  .. 'xrdb -merge <<< "awesome.started:true";'
-  -- list each of your autostart commands, followed by ; inside double quotes, followed by ..
-  .. "dex --environment Awesome --autostart;"
-  .. "lxsession &"
-  .. "xset r rate 150 60"
+	'if (xrdb -query | grep -q "^awesome\\.started:\\s*true$"); then exit; fi;'
+	.. 'xrdb -merge <<< "awesome.started:true";'
+	-- list each of your autostart commands, followed by ; inside double quotes, followed by ..
+	.. "dex --environment Awesome --autostart;"
+	.. "lxsession &"
+	.. "xset r rate 150 60"
 )
