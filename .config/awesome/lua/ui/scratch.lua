f7e2133d5220445bdfa92c8ae29ea8b53a01e812
Nour El Din Shady <rhasoldy@gmail.com>
2023-07-24 05:06:48 +0300

    Lowered scratchpads

--- .config/awesome/lua/ui/scratch.lua
+++ .config/awesome/lua/ui/scratch.lua
@@ -1,28 +1,28 @@
 local bling = require("bling")
 
 Spad = bling.module.scratchpad({
-  command = "kitty --class spad",
-  rule = { class = "spad" },
-  sticky = true,
-  floating = true,
-  geometry = { x = 250, y = 70, height = 900, width = 1400 },
-  reapply = true,
+	command = "kitty --class spad",
+	rule = { class = "spad" },
+	sticky = true,
+	floating = true,
+	geometry = { x = 250, y = 95, height = 900, width = 1400 },
+	reapply = true,
 })
 
 Pop = bling.module.scratchpad({
-  command = "kitty --class pop",
-  rule = { class = "pop" },
-  sticky = true,
-  floating = true,
-  geometry = { x = 580, y = 20, height = 100, width = 800 },
-  reapply = true,
+	command = "kitty --class pop",
+	rule = { class = "pop" },
+	sticky = true,
+	floating = true,
+	geometry = { x = 580, y = 30, height = 100, width = 800 },
+	reapply = true,
 })
 
 Spot = bling.module.scratchpad({
-  command = "spotify",
-  rule = { class = "Spotify" },
-  sticky = true,
-  floating = true,
-  geometry = { x = 250, y = 70, height = 900, width = 1400 },
-  reapply = true,
+	command = "spotify",
+	rule = { class = "Spotify" },
+	sticky = true,
+	floating = true,
+	geometry = { x = 250, y = 95, height = 900, width = 1400 },
+	reapply = true,
 })
