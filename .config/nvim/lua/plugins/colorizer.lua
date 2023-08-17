bdb9dd6e2a0c9afffba3c46aab84878e98fc7c41
Nour El Din Shady <rhasoldy@gmail.com>
2023-08-17 05:37:42 +0300

    now using nvchads colorizer lol sorry mb

--- .config/nvim/lua/plugins/colorizer.lua
+++ .config/nvim/lua/plugins/colorizer.lua
@@ -1,30 +1,34 @@
 return {
 	{
-		"brenoprata10/nvim-highlight-colors",
+		"NvChad/nvim-colorizer.lua",
 		event = "VeryLazy",
 		opts = {
-			filetypes = { "*" },
+			filetypes = {
+				"*",
+				cmp_docs = { always_update = true },
+				cmp_menu = { always_update = true },
+				css = {
+					css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+					css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+				},
+				scss = {
+					css = true,                               -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+					css_fn = true,                            -- Enable all CSS *functions*: rgb_fn, hsl_fn
+					sass = { enable = true, parsers = { "css" } }, -- Enable sass colors
+				},
+			},
 			user_default_options = {
-				RGB = true, -- #RGB hex codes
-				RRGGBB = true, -- #RRGGBB hex codes
-				names = true, -- "Name" codes like Blue or blue
-				RRGGBBAA = true, -- #RRGGBBAA hex codes
-				AARRGGBB = true, -- 0xAARRGGBB hex codes
-				rgb_fn = true, -- CSS rgb() or rgba() functions
-				hsl_fn = true, -- CSS hsl() and hsla() functions
-				css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-				css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+				RGB = true,       -- #RGB hex codes
+				RRGGBB = true,    -- #RRGGBB hex codes
+				names = true,     -- "Name" codes like Blue or blue
+				RRGGBBAA = true,  -- #RRGGBBAA hex codes
+				AARRGGBB = true,  -- 0xAARRGGBB hex codes
+				rgb_fn = true,    -- CSS rgb() or rgba() functions
+				hsl_fn = true,    -- CSS hsl() and hsla() functions
 				-- Available modes for `mode`: foreground, background,  virtualtext
-				mode = "background", -- Set the display mode.
-				-- Available methods are false / true / "normal" / "lsp" / "both"
-				-- True is same as normal
-				tailwind = true, -- Enable tailwind colors
-				-- parsers can contain values used in |user_default_options|
-				sass = { enable = true, parsers = { "css" } }, -- Enable sass colors
+				mode = "virtualtext", -- Set the display mode.
+				tailwind = true,  -- Enable tailwind colors
 				virtualtext = "■",
-				-- update color values even if buffer is not focused
-				-- example use: cmp_menu, cmp_docs
-				always_update = false,
 			},
 			-- all the sub-options of filetypes apply to buftypes
 			buftypes = {},
